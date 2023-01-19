0b8162bec240d270ea1900e88e7620b3c26c5a91
haidi nata <haidinurhadinata@gmail.com>
2023-01-19 23:38:44 +0800

    pakai virtual bisa akses ms access

--- index.py
+++ index.py
@@ -7,24 +7,29 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    cnxn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=\\TEST-PC\sharing\testing.mdb;')
 
-    df = pd.read_sql("SELECT TOP 1 * FROM campaign_table ct ORDER BY ID DESC", cnxn) 
-    
+@app.route("/")
+def index():
+    cnxn = pyodbc.connect(
+        r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=\\TEST-PC\sharing\testing.mdb;"
+    )
+
+    df = pd.read_sql("SELECT TOP 1 * FROM campaign_table ct ORDER BY ID DESC", cnxn)
+
     # print(df.head())
-    
-    data = df.to_json(orient='records')
-    
-    if(type(data) == str):
+
+    data = df.to_json(orient="records")
+
+    if type(data) == str:
         return json.loads(data)
     else:
         return "not"
-    
-@socketio.on('connect')
-def test_connect():
-    emit('after connect', {'data': 'terkoneksi'})
 
-if __name__ == '__main__':
-    socketio.run(app, host='localhost', debug=True)
+
+@socketio.on("connect")
+def test_connect():
+    emit("after connect", {"data": "terkoneksi"})
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="localhost", debug=True)
